2a3b52389d2bbc190a66dbbd2724be2479d7b957
Rodrigo Gomez <rmgomez.dev@protonmail.com>
2020-01-31 23:30:02 +0100

    Add GameInput script and test scene

---
+++ Assets/GameInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class GameInput : MonoBehaviour
+{
+
+    [SerializeField] private LayerMask _tileMask;
+    private Camera _camera;
+    
+    void Start()
+    {
+        _camera = GetComponent<Camera>();
+        if (_camera == null) Debug.LogError("No camera  found, please attach script to camera");
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            DetectTouch();
+        }
+    }
+
+    void DetectTouch()
+    {
+        //Debug.Log("Player Clicked");
+
+        var ray = _camera.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out var hit, 200, _tileMask))
+        {
+            Debug.Log($"Hit {hit.collider.gameObject.name}");
+        }
+
+    }
+}
