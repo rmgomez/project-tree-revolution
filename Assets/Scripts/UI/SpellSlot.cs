eb144a4929479f7583ce81bdce019401b9467446
Rodrigo Gomez <rmgomez.dev@protonmail.com>
2020-02-01 16:23:15 +0100

    Create Spell data and spell slot for UI

---
+++ Assets/Scripts/UI/SpellSlot.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpellSlot : MonoBehaviour
+{
+    public Image Icon;
+    public Toggle ToggleButton;
+    public Text NaturePointsText;
+    public Text SpellNameText;
+
+    private int _slotIndex;
+
+    private SpellData _data;
+    public void Init(SpellData data, ToggleGroup toggleGroup)
+    {
+        _data = data;
+        Icon.sprite = data.ButtonIcon;
+        NaturePointsText.text = data.NaturePoints.ToString("D2");
+        SpellNameText.text = data.SpellName;
+        ToggleButton.group = toggleGroup;
+    }
+
+    public void SelectSpell()
+    {
+        Debug.Log($"Is Slot {gameObject.name} on: {ToggleButton.isOn}");
+        if (ToggleButton.isOn) LevelManager.Instance.SelectedSpellID = _data.SpellID;
+    }
+}
