718bd83174bde75c60a0feb54befb881fae6f346
Rodrigo Gomez <rmgomez.dev@protonmail.com>
2020-02-01 22:27:20 +0100

    Add Life To Enemies that need it

---
+++ Assets/LookAtCamera.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Update()
+    {
+        if(Camera.main != null) transform.LookAt(Camera.main.transform.position, -Vector3.up);
+    }
+}
