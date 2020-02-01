43a5e4b75868ee8f401c96617b8437a6125debf5
Rodrigo Gomez <rmgomez.dev@protonmail.com>
2020-02-01 16:24:27 +0100

    Improve GameInput

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -5,24 +5,38 @@ public class GameInput : MonoBehaviour
 
     [SerializeField] private LayerMask _tileMask;
     private Camera _camera;
+
+    private bool _allowInput;
     
-    void Start()
+    private void OnEnable()
+    {
+        LevelManager.OnPlayerTurnStarted += OnPlayerTurnStarted;
+        LevelManager.OnPlayerTurnCompleted += OnPlayerTurnCompleted;
+    }
+
+    private void OnDisable()
+    {
+        LevelManager.OnPlayerTurnStarted -= OnPlayerTurnStarted;
+        LevelManager.OnPlayerTurnCompleted -= OnPlayerTurnCompleted;
+    }
+
+    private void Start()
     {
         _camera = GetComponent<Camera>();
         if (_camera == null) Debug.LogError("No camera  found, please attach script to camera");
     }
 
-    void Update()
+    private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && _allowInput)
         {
             DetectTouch();
         }
     }
 
-    void DetectTouch()
+    private void DetectTouch()
     {
-        //Debug.Log("Player Clicked");
+        Debug.Log("Player Clicked");
 
         var ray = _camera.ScreenPointToRay(Input.mousePosition);
 
@@ -31,5 +45,22 @@ public class GameInput : MonoBehaviour
             Debug.Log($"Hit {hit.collider.gameObject.name}");
         }
 
+    }
+
+    private void SetTile(Tile tile)
+    {
+        //Tell tile to spawn
+        //Tell tile to heal
+    }
+
+    private void OnPlayerTurnStarted()
+    {
+        _allowInput = true;
+    }
+
+
+    private void OnPlayerTurnCompleted()
+    {
+        _allowInput = false;
     }
 }
